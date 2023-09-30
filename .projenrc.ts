a52afbd6d1ba729aaac719227b18f260800ce81a
Matthew Bonig <matthew.bonig@gmail.com>
2023-09-30 13:22:24 -0600

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,13 @@
+import { awscdk } from 'projen';
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'reinvent',
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
